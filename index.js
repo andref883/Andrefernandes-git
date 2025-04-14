23c7595b9199e1f6cc2ec1f80deb1771d78494b1
unknown <andre.fernandes.antunes13@gmail.com>
2025-04-14 08:35:36 -0300

    negativo

---
+++ index.js
@@ -0,0 +1,3 @@
+let nome =prompt("digite seu nome"); // variável perceberá o nome do usuário. O comando receberá a string com o nome do usuário.
+let idade =parseInt(prompt("digite sua idade")); // a variável idade receberá a idade do usuário. O comando prompt será parseado (convertido) em um valor numérico inteiro.
+let dolar =parseFloat(prompt("digite quantos dólares você tem")); // a variável dolar receberá o valor em dolar expresso pelo usuário. O prompt receberá  uma string que vai ser convertida em float(real).
